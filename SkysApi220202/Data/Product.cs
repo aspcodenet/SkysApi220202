dd46a599521263dbf91ca37309a2bfa90d23bb93
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-02-02 12:43:08 +0100

    Add project files.

---
+++ SkysApi220202/Data/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkysApi220202.Data;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    [MaxLength(20)]
+    public string Color { get; set; }
+    [MaxLength(15)]
+    public string Ean13 { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastBought { get; set; }
+
+    public int PopularityPercent { get; set; }
+}
---
+++ SkysApi220202/Program.cs
@@ -0,0 +1,74 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using SkysApi220202.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+var SecretKey = "znxmb4saddas";
+var Issuer = "stefans ab";
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+    
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = false,
+            ValidIssuer = Issuer,
+            ValidAudience = Issuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey))
+        };
+    });
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+
+builder.Services.AddTransient<DataInitializer>();
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetService<DataInitializer>().SeedData();
+}
+
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
